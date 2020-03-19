40d42add44e47518b9fcc7ad1f1225fcf0d2076b
Emsaso <34371682+Emsaso@users.noreply.github.com>
2020-03-19 12:21:51 +0100

    x

--- HellenesKoordinatOppgave/MainPage.xaml.cs
+++ HellenesKoordinatOppgave/MainPage.xaml.cs
@@ -1,100 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
 using MetadataExtractor;
 using Directory = MetadataExtractor.Directory;
-using Image = Windows.UI.Xaml.Controls.Image;
-
 
 namespace HellenesKoordinatOppgave
 {
     public sealed partial class MainPage : Page
     {
-
         public MainPage()
         {
             this.InitializeComponent();
-            GetImageData();
+            GetPictureAndMeta();
         }
 
-        public void GetImageData()
+        public void GetPictureAndMeta()
         {
-            Image image1 = new Image();
-            Image image2 = new Image();
-            Image image3 = new Image();
-            StackPanel1.Children.Add(image1);
-            StackPanel3.Children.Add(image2);
-            StackPanel5.Children.Add(image3);
-            image1.Source = new BitmapImage(new Uri(image1.BaseUri, "Assets/DJI_0025.JPG"));
-            image2.Source = new BitmapImage(new Uri(image2.BaseUri, "Assets/DJI_0027.JPG"));
-            image3.Source = new BitmapImage(new Uri(image3.BaseUri, "Assets/DJI_0028.JPG"));
-
-            TextBlock txt1 = new TextBlock();
-            TextBlock txt2 = new TextBlock();
-            TextBlock txt3 = new TextBlock();
-            StackPanel2.Children.Add(txt1);
-            StackPanel4.Children.Add(txt2);
-            StackPanel6.Children.Add(txt3);
-
-
-            IEnumerable<Directory> directories1 = ImageMetadataReader.ReadMetadata("Assets\\DJI_0025.JPG");
-            IEnumerable<Directory> directories2 = ImageMetadataReader.ReadMetadata("Assets\\DJI_0027.JPG");
-            IEnumerable<Directory> directories3 = ImageMetadataReader.ReadMetadata("Assets\\DJI_0028.JPG");
-            string temp1 = "";
-            string temp2 = "";
-            string temp3 = "";
-            foreach (var directory in directories1)
-                foreach (var tag in directory.Tags)
-                {
-                    if (tag.Name.Contains("Latitude") && !tag.Name.Contains("Ref")
-                        || tag.Name.Contains("Longitude") && !tag.Name.Contains("Ref")
-                        || tag.Name.Contains("Altitude") && !tag.Name.Contains("Ref"))
-                    {
-                        var output = ($"{tag.Name} = {tag.Description}\n").Remove(0, 4);
-                        txt1.Text += output;
-                        temp1 += output;
-                    }
-                }
-            foreach (var directory in directories2)
-                foreach (var tag in directory.Tags)
-                {
-                    if (tag.Name.Contains("Latitude") && !tag.Name.Contains("Ref")
-                        || tag.Name.Contains("Longitude") && !tag.Name.Contains("Ref")
-                        || tag.Name.Contains("Altitude") && !tag.Name.Contains("Ref"))
-                    {
-                        var output = ($"{tag.Name} = {tag.Description}\n").Remove(0, 4);
-                        txt2.Text += output;
-                        temp2 += output;
-                    }
-                }
-            foreach (var directory in directories3)
-                foreach (var tag in directory.Tags)
-                {
-                    if (tag.Name.Contains("Latitude") && !tag.Name.Contains("Ref")
-                        || tag.Name.Contains("Longitude") && !tag.Name.Contains("Ref")
-                        || tag.Name.Contains("Altitude") && !tag.Name.Contains("Ref"))
-                    {
-                        var output = ($"{tag.Name} = {tag.Description}\n").Remove(0, 4);
-                        txt3.Text += output;
-                        temp3 += output;
-                    }
-                }
-
-            if (temp1 == temp2)
+            foreach (var item in System.IO.Directory.GetFiles("Assets\\CoordinateImages", "*.*").ToList())
             {
-                txt1.Text += "\nPicture 1 & 2 are taken at the same place";
-                txt2.Text += "\nPicture 2 & 1 are taken at the same place";
-            }
-            if (temp2 == temp3)
-            {
-                txt2.Text += "\nPicture 2 & 3 are taken at the same place";
-                txt3.Text += "\nPicture 3 & 2 are taken at the same place";
-            }
-            if (temp3 == temp1)
-            {
-                txt3.Text += "\nPicture 3 & 1 are taken at the same place";
-                txt1.Text += "\nPicture 1 & 3 are taken at the same place";
+
+                string output = null;
+
+                IEnumerable<Directory> directories = ImageMetadataReader.ReadMetadata(item);
+                foreach (var directory in directories) foreach (var tag in directory.Tags)
+                        if (tag.Name.Contains("Latitude") && !tag.Name.Contains("Ref")
+                                || tag.Name.Contains("Longitude") && !tag.Name.Contains("Ref")
+                                || tag.Name.Contains("Altitude") && !tag.Name.Contains("Ref"))
+                        {
+                            output += ($"{tag.Name} = {tag.Description}\n").Remove(0, 4);
+                        }
+
+                Button selectionButton1 = new Button();
+                selectionButton1.Content = item.Remove(0, 24);
+                selectionButton1.Click += (sender, args) => Image1.Source = new BitmapImage(new Uri(BaseUri, item));
+                selectionButton1.Click += (sender, args) => Description1.Text = output ?? throw new InvalidOperationException();
+                selectionButton1.HorizontalAlignment = HorizontalAlignment.Center;
+                Button selectionButton2 = new Button();
+                selectionButton2.Content = item.Remove(0, 24);
+                selectionButton2.Click += (sender, args) => Image2.Source = new BitmapImage(new Uri(BaseUri, item));
+                selectionButton2.Click += (sender, args) => Description2.Text = output ?? throw new InvalidOperationException();
+                selectionButton2.HorizontalAlignment = HorizontalAlignment.Center;
+
+                StackPanel1.Children.Add(selectionButton1);
+                StackPanel2.Children.Add(selectionButton2);
             }
         }
     }
