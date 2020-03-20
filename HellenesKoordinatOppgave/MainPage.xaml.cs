078df9b3127bc7f440cf56ee031212452cfa8dea
Emsaso <34371682+Emsaso@users.noreply.github.com>
2020-03-20 11:22:25 +0100

    x

--- HellenesKoordinatOppgave/MainPage.xaml.cs
+++ HellenesKoordinatOppgave/MainPage.xaml.cs
@@ -25,27 +25,46 @@ namespace HellenesKoordinatOppgave
                 string output = null;
 
                 IEnumerable<Directory> directories = ImageMetadataReader.ReadMetadata(item);
-                foreach (var directory in directories) foreach (var tag in directory.Tags)
+                foreach (var directory in directories)
+                {
+                    foreach (var tag in directory.Tags)
+                    {
                         if (tag.Name.Contains("Latitude") && !tag.Name.Contains("Ref")
                                 || tag.Name.Contains("Longitude") && !tag.Name.Contains("Ref")
                                 || tag.Name.Contains("Altitude") && !tag.Name.Contains("Ref"))
                         {
                             output += ($"{tag.Name} = {tag.Description}\n").Remove(0, 4);
                         }
+                    }
+                }
 
                 Button selectionButton1 = new Button();
                 selectionButton1.Content = item.Remove(0, 24);
                 selectionButton1.Click += (sender, args) => Image1.Source = new BitmapImage(new Uri(BaseUri, item));
                 selectionButton1.Click += (sender, args) => Description1.Text = output ?? throw new InvalidOperationException();
+                selectionButton1.Click += CheckForSimilarity;
                 selectionButton1.HorizontalAlignment = HorizontalAlignment.Center;
                 Button selectionButton2 = new Button();
                 selectionButton2.Content = item.Remove(0, 24);
                 selectionButton2.Click += (sender, args) => Image2.Source = new BitmapImage(new Uri(BaseUri, item));
                 selectionButton2.Click += (sender, args) => Description2.Text = output ?? throw new InvalidOperationException();
+                selectionButton2.Click += CheckForSimilarity;
                 selectionButton2.HorizontalAlignment = HorizontalAlignment.Center;
 
                 StackPanel1.Children.Add(selectionButton1);
                 StackPanel2.Children.Add(selectionButton2);
+            }
+        }
+
+        private void CheckForSimilarity(object sender, RoutedEventArgs e)
+        {
+            if (Description1.Text == Description2.Text)
+            {
+                EqualityChecker.Text = "Pictures are taken in the same spot.";
+            }
+            else
+            {
+                EqualityChecker.Text = "";
             }
         }
     }
